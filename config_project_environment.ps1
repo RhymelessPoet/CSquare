642e2672a6b46910eb1c0378931813f7a25aa229
lxtao@tju.edu.cn <lxtao@tju.edu.cn>
2025-03-22 22:09:49 +0800

    Add powershell script to config qt6 cmake path

---
+++ config_project_environment.ps1
@@ -0,0 +1,18 @@
+Param
+(
+    [string]$Qt6_Path = "D:\Qt\6.7.0\msvc2019_64\lib\cmake"
+)
+
+# 判断当前是否管理员权限
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    # 获取当前脚本路径
+    $scriptPath = $MyInvocation.MyCommand.Definition
+    # 以管理员身份启动新进程并传递参数
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
+    Exit  # 退出当前非管理员进程
+}
+# 后续代码将以管理员权限执行
+Write-Host "Currently have administrator privileges!" -ForegroundColor Green
+
+[System.Environment]::SetEnvironmentVariable("CMAKE_PREFIX_PATH", "$Qt6_Path", "Machine")
